8f37c575d01529e4868915c2f61bc1c17d89a94d
itsme-swe <me.harshmehra07@gmail.com>
2025-01-14 05:41:27 +0530

    New Repo

---
+++ 1_RectangleClass.cpp
@@ -0,0 +1,33 @@
+#include <iostream> 
+
+using namespace std; 
+
+class Rectangle 
+{
+  public:
+  int length;
+
+  int breadth;
+
+  int area() {
+    return length * breadth;
+  }
+
+};
+
+int main(){
+
+  Rectangle r1, r2;
+
+  r1.length = 10;
+  r1.breadth = 5;
+
+  r2.length = 5;
+  r2.breadth = 20;
+
+  cout << "The area of rectangle r1 is " << r1.area() << endl;
+
+  cout << "The area of rectangle r2 is " << r2.area() << endl;
+
+  return 0;
+}
---
+++ 2_ObjectInHeap.cpp
@@ -0,0 +1,30 @@
+#include <iostream> 
+
+using namespace std; 
+
+class Rectangle
+{
+  public:
+  int length;
+  int breadth;
+
+  int area() {
+    
+    return length * breadth;
+
+  }
+
+};
+
+int main(){
+
+  // This is how we create an object inside Heap memory using pointer
+  Rectangle *ptr = new Rectangle();
+
+  ptr-> length = 10;
+  ptr-> breadth = 5;
+
+  cout << "Area of rectangle is " << ptr-> area(); 
+
+  return 0;
+}
