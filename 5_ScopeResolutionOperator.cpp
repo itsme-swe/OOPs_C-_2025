122ba79c29cee905f1d634c0640d02166b188beb
itsme-swe <me.harshmehra07@gmail.com>
2025-01-14 09:09:43 +0530

    update

---
+++ 5_ScopeResolutionOperator.cpp
@@ -0,0 +1,54 @@
+#include <iostream> 
+
+using namespace std; 
+
+class Rectangle
+{
+
+  private:
+  //🔸 Properties
+  int length;
+  int breadth;
+
+  public:
+  //🔸 Prototype of Parameterized Constructor
+  Rectangle(int l, int b);
+  
+  //🔸 Prototype of Mutators
+  void setLength(int l);
+  void setBreadth(int b);
+
+  //🔸Prototype of Area function
+  int area();
+
+
+};
+
+//🔸 Parameterized Constructor body using scope resolution operator
+Rectangle:: Rectangle(int l, int b) {
+  length = l;
+  breadth = b;
+}
+
+void Rectangle::setLength(int l) {
+  length = l;
+}
+
+void Rectangle:: setBreadth(int b) {
+  breadth = b;
+}
+
+int Rectangle:: area() {
+  return length * breadth;
+}
+
+int main(){
+
+  Rectangle r1(10,10);
+
+  cout << "The area of rectangle is " << r1.area() << endl;
+
+  return 0;
+}
+
+//o/p: The area of rectangle is 100
