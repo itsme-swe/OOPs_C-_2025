0007e66a25ba4e900dcd56000fcad8e67490d3a6
itsme-swe <me.harshmehra07@gmail.com>
2025-01-15 18:08:17 +0530

    update

---
+++ Polymorphism/FunctionOverriding.cpp
@@ -0,0 +1,31 @@
+#include <iostream> 
+
+using namespace std; 
+
+class Base
+{
+
+  public:
+  void display() {
+    cout << "Display of base " << endl;
+  }
+
+};
+
+class Derived : public Base
+{
+  public:
+  void display() {
+    cout << "Display of Derived Class";
+  }
+
+};
+
+int main(){
+
+  Derived b; 
+  b.display();
+  
+
+  return 0;
+}
